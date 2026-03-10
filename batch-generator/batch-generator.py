bcbba224fbc34b4f3f6b1eca169fb709c9be6f08
Bìh Emmm <109718501+1zuki@users.noreply.github.com>
2026-03-10 11:09:15 +0700

    Add solution for batch-generator

---
+++ batch-generator/batch-generator.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def batch_generator(X, y, batch_size, rng=None, drop_last=False):
+    """
+    Randomly shuffle a dataset and yield mini-batches (X_batch, y_batch).
+    """
+    # Write code here
+    X = np.asarray(X)
+    y = np.asarray(y)
+    N = len(X)
+    indices = np.arange(N)
+    
+    if rng is not None:
+        rng.shuffle(indices)
+    else:
+        np.random.shuffle(indices)
+    
+    for start in range(0, N - (batch_size if drop_last else 0), batch_size):
+        end = start + batch_size
+        X_batch = X[indices[start:end]]
+        y_batch = y[indices[start:end]]
+        yield (X_batch, y_batch)
