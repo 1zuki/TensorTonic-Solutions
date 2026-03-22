e2c4cfcff9e32cf5dbfc5750f1460eb8c3200194
Bìh Emmm <109718501+1zuki@users.noreply.github.com>
2026-03-23 00:23:51 +0700

    Update solution for he-initialization

--- he-initialization/he-initialization.py
+++ he-initialization/he-initialization.py
@@ -5,9 +5,7 @@ def he_initialization(W, fan_in):
     # Write code here
     W = np.asarray(W)
     fan_in = np.asarray(fan_in)
-    
-    if not True in np.where((0, 1)):
-        return W
+
     
     limit = np.sqrt(6 / fan_in)
     new_W = W * (2 * limit) - limit
