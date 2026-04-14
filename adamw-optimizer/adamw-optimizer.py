8245ca5c1de0b66c26eb272b6956c04abdbdc48a
Bìh Emmm <109718501+1zuki@users.noreply.github.com>
2026-04-14 11:30:29 +0700

    Add solution for adamw-optimizer

---
+++ adamw-optimizer/adamw-optimizer.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def adamw_step(w, m, v, grad, lr=0.001, beta1=0.9, beta2=0.999, weight_decay=0.01, eps=1e-8):
+    """
+    Perform one AdamW update step.
+    """
+    # Write code here
+    w = np.asarray(w, dtype=float)
+    m = np.asarray(m, dtype=float)
+    v = np.asarray(v, dtype=float)
+    grad = np.asarray(grad)
+    
+    m_new = beta1 * m + (1 - beta1) * grad
+    v_new = beta2 * v + (1 - beta2) * grad ** 2
+    w_new = w - lr * (weight_decay * w) - lr * m_new / (v_new ** (1 / 2) + eps)
+
+    return w_new, m_new, v_new
