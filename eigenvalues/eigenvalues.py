765a3950c3b55a6d8869ac47aa3201664b75cb16
Izukia <109718501+1zuki@users.noreply.github.com>
2026-09-06 02:43:59 +0700

    Solve eigenvalues

--- eigenvalues/eigenvalues.py
+++ eigenvalues/eigenvalues.py
@@ -5,8 +5,4 @@ def calculate_eigenvalues(matrix: list) -> np.ndarray:
     Returns a sorted NumPy array of real eigenvalues.
     """
     # Write code here
-    matrix = np.asarray(matrix, dtype=float)
-    eigv = np.linalg.eigvals(matrix)
-    eigv = np.sort(eigv.real)
-
-    return eigv
+    return np.sort(np.linalg.eigvals(matrix).real)
